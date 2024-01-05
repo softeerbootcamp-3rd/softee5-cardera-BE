95be073b643bbf1fd3e110b7765c4cdb1a0d0a05
swkim0911 <kimsungwon364@gmail.com>
2024-01-05 17:10:06 +0900

    feat: 프로젝트 세팅

---
+++ src/main/java/com/warmingup/cardera/CarderaApplication.java
@@ -0,0 +1,13 @@
+package com.warmingup.cardera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarderaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarderaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/warmingup/cardera/CarderaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warmingup.cardera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarderaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
