7692bfb64b27e74dde9f7c1462e9c60df0da545f
Yoon-Suji <jidl88@ewhain.net>
2024-01-06 13:25:52 +0900

    feat: create UserChoice entity

---
+++ src/main/java/com/warmingup/cardera/domain/entity/UserChoice.java
@@ -0,0 +1,23 @@
+package com.warmingup.cardera.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+@Table
+public class UserChoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private double multiple;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private int choiceCount = 0;
+}
